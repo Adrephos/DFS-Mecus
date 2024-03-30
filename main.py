3a337c1eca997da53174f3b954c7089e87218b1f
Adrephos <adrephos@gmail.com>
2024-03-29 19:43:36 -0500

    feat: add function to reconstruct a file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-def split_file(path: str, chunk_size:int):
+def split_file(path: str, chunk_size: int):
     fileR = open(path, "rb")
     chunk = 0
     byte = fileR.read(chunk_size)
@@ -20,5 +20,39 @@ def split_file(path: str, chunk_size:int):
 
         chunk += 1
 
-img = input("File path (from root): ")
-split_file(img, 1400*1024)
+
+def rebuild_file(name: str):
+    try:
+        os.mkdir("./reconstruct")
+    except:
+        print("./reconstruct/ already exists")
+
+    fileM = open("./reconstruct/" + name, "wb")
+    chunk = 0
+
+    fileName = "./chunks/" + name + ".chunk" + str(chunk)
+    try:
+        fileTemp = open(fileName, "rb")
+    except:
+        print("not a valid file to reconstruct")
+        exit()
+    while fileTemp:
+        print(f'- chunk #{chunk} done')
+        byte = fileTemp.read()
+        fileM.write(byte)
+
+        chunk += 1
+
+        fileName = "./chunks/" + name + ".chunk" + str(chunk)
+        try:
+            fileTemp = open(fileName, "rb")
+        except:
+            break
+
+
+print("Split file")
+file = input("File path (from root): ")
+split_file(file, 1400*1024)
+
+print("\nRebuild file")
+rebuild_file(file.split("/")[-1])
