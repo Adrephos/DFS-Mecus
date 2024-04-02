15a88fa84f4bc60e2baab886cff3ae3a5710f22b
Adrephos <adrephos@gmail.com>
2024-04-01 19:48:23 -0500

    feat: add hashing to passwords

--- name-node/name_node.py
+++ name-node/name_node.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from tinydb import TinyDB, Query
+import bcrypt
 
 # Inicialización de la base de datos y la tabla
 db = TinyDB('db.json')
@@ -9,6 +10,13 @@ clients = db.table('clients')
 # Inicialización y configuración de Flask
 # Server
 app = Flask(__name__)
+
+
+def hash_and_salt(password: str):
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed.decode('utf-8')
 
 
 @app.route('/register', methods=['POST'])
@@ -23,7 +31,8 @@ def register():
         if user:
             response = {'response': 'Username is already in use'}
         else:
-            clients.insert({'name': name, 'password': password, 'ip': ip})
+            clients.insert(
+                {'name': name, 'password': hash_and_salt(password), 'ip': ip})
             response = {'response': 'Successful registration'}
     else:
         response = {'response': 'Unknown message.'}
@@ -39,8 +48,12 @@ def login():
     ip = data.get('ip')
 
     if name and password and ip:
-        user = clients.get((Query().name == name) & (Query().password == password))
+        user = clients.get(Query().name == name)
+
         if user:
+            hash = str(user.get('password')).encode('utf-8')
+            if not bcrypt.checkpw(password.encode('utf-8'), hash):
+                response = {'response': 'Invalid username or password'}
             if user['ip'] != ip:
                 clients.update({'ip': ip}, doc_ids=[user.doc_id])
 
